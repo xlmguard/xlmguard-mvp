a69ba73a9d5fbc24df9f9fe194cc253178ff2e5d
pazzaglini-mvp <pazzaglini76@gmail.com>
2025-05-17 07:56:50 -0400

    Commit and rebuild

--- src/firebase.js
+++ src/firebase.js
@@ -3,27 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyC3hxvl6JGceZhOwiaH1O7pNo92pWMeEuQ",
+  apiKey: "AIzaSyChxvbl6JceZh0iaH107pNo92pWMeEuqQ",
   authDomain: "xlmguard.firebaseapp.com",
   projectId: "xlmguard",
-  storageBucket: "xlmguard.firebasestorage.app",
-  messagingSenderId: "369260793839",
-  appId: "1:369260793839:web:0aa74cbe7a3d3dfbb3701a",
-  measurementId: "G-3SFLTB94BV"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+  storageBucket: "xlmguard.appspot.com",
+  messagingSenderId: "369260730839",
+  appId: "1:369260730839:web:00a7d6be7a3d3dfbb3701a",
+  measurementId: "G-3FSLTB94BV"
 };
 
 const app = initializeApp(firebaseConfig);
