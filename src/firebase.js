d5ceb2813fb200c1ee5a904631f7b39dfd6ef377
pazzaglini-mvp <pazzaglini76@gmail.com>
2025-05-18 19:24:27 -0400

    deploy

--- src/firebase.js
+++ src/firebase.js
@@ -16,6 +16,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore(app); // ✅ You forgot this line!
+  
 
   
 // ✅ This is the correct and only export line
