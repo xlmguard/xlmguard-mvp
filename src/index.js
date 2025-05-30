56e939ff0a5f9ff5261be3a49210c1aa19418ed1
pazzaglini-mvp <pazzaglini76@gmail.com>
2025-05-30 15:42:47 -0400

    commit and push

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
 
 
