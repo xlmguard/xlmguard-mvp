0677fbe2d8044c8e513ad5ace3791666a6c6a09b
pazzaglini-mvp <pazzaglini76@gmail.com>
2025-05-30 15:59:19 -0400

    commit and push

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth"; // ← ADD THIS
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC3hxvl6JcceZhOwiaH107pNo92pWMeEuQ",
+  apiKey: "AIzaSyC3hxvl6JGceZhOwiaH1O7pNo92pWMeEuQ",
   authDomain: "xlmguard.firebaseapp.com",
   projectId: "xlmguard",
-  storageBucket: "xlmguard.appspot.com",
+  storageBucket: "xlmguard.appspot.com", // ✅ FIXED
   messagingSenderId: "369260793839",
   appId: "1:369260793839:web:0aa74cbe7a3d3dfbb3701a",
   measurementId: "G-3SFLTB94BV"
@@ -16,7 +16,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const analytics = getAnalytics(app);
-const auth = getAuth(app); // ← ADD THIS
+const auth = getAuth(app);
 
-// ✅ Exporting everything needed
 export { app, db, analytics, auth };
+
