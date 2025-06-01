3a54bb1fa5c110b850dd48924fada4a13476c2d8
pazzaglini-mvp <pazzaglini76@gmail.com>
2025-06-01 12:09:29 -0400

    commit and push

--- src/SubmissionForm.js
+++ src/SubmissionForm.js
@@ -1,4 +1,4 @@
-// App.js
+// SubmissionForm.js
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { onAuthStateChanged } from 'firebase/auth';
@@ -65,5 +65,6 @@ function App() {
 }
 
 export default App;
+
 
 
