7a17bdb5596528e43572220d66aed49f54dae68c
pazzaglini-mvp <pazzaglini76@gmail.com>
2025-07-02 11:03:23 -0400

    commit and push

--- src/PaymentPage.js
+++ src/PaymentPage.js
@@ -1,5 +1,5 @@
 // PaymentPage.js
-import { QRCode } from "qrcode.react";
+import { QR } from "qrcode.react";
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { auth, db } from './firebase';
