013f4036d92dc1f416581cacaf9c4a57f1f06969
pazzaglini-mvp <pazzaglini76@gmail.com>
2025-07-13 15:08:46 -0400

    commit and push

--- src/firebase.js
+++ src/firebase.js
@@ -9,17 +9,18 @@ const firebaseConfig = {
   apiKey: "AIzaSyC3hxvl6JGceZhOwiaH1O7pNo92pWMeEuQ",
   authDomain: "xlmguard.firebaseapp.com",
   projectId: "xlmguard",
-  storageBucket: "xlmguard.appspot.com", // ✅ canonical bucket
+  storageBucket: "xlmguard.appspot.com",
   messagingSenderId: "369260793839",
   appId: "1:369260793839:web:0aa74cbe7a3d3dfbb3701a",
   measurementId: "G-3SFLTB94BV"
-} ;
+};
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+
 
 
 
